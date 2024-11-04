c660c21af3d45c88cdbcd2aed61882fc9626a164
Lorenzo De Simone <lsimone24@fed.idserve.net>
2024-11-04 10:36:39 +0100

    Initial Commit

---
+++ workshoptest/workshoptestApp.swift
@@ -0,0 +1,17 @@
+//
+//  workshoptestApp.swift
+//  workshoptest
+//
+//  Created by Lorenzo De Simone on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct workshoptestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ workshoptestTests/workshoptestTests.swift
@@ -0,0 +1,17 @@
+//
+//  workshoptestTests.swift
+//  workshoptestTests
+//
+//  Created by Lorenzo De Simone on 04/11/24.
+//
+
+import Testing
+@testable import workshoptest
+
+struct workshoptestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
